96fbf7f1cd49e461b278d65637b6633faa55e2ff
imtahirabatool <btahira183@gmail.com>
2024-05-31 15:53:28 +0500

    Question added!

--- Arrays/productOfArrayExceptItself.cpp
+++ Arrays/productOfArrayExceptItself.cpp
@@ -1,4 +1,6 @@
-#include <iostream>
+// Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+// You must write an algorithm that runs in O(n) time.
+#include<iostream>
 #include <vector>
 using namespace std;
 
